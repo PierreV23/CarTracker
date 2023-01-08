d6829f1e443b7257053ba965d6d386850bcea1b9
Pièrre <77776198+PierreV23@users.noreply.github.com>
2023-01-08 13:57:14 +0100

    test

---
+++ CarTracker/CarTracker.cpp
@@ -0,0 +1,83 @@
+#include "pch.h"
+#include "CarTracker.h"
+
+
+BAKKESMOD_PLUGIN(CarTracker, "write a plugin description here", plugin_version, PLUGINTYPE_FREEPLAY)
+
+std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
+bool coolEnabled = false;
+
+void CarTracker::onLoad()
+{
+	_globalCvarManager = cvarManager;
+	LOG("CarTracker - Plugin loaded!");
+	// !! Enable debug logging by setting DEBUG_LOG = true in logging.h !!
+	//DEBUGLOG("CarTracker debug mode enabled");
+
+	// LOG and DEBUGLOG use fmt format strings https://fmt.dev/latest/index.html
+	//DEBUGLOG("1 = {}, 2 = {}, pi = {}, false != {}", "one", 2, 3.14, true);
+
+	//cvarManager->registerNotifier("my_aweseome_notifier", [&](std::vector<std::string> args) {
+	//	LOG("Hello notifier!");
+	//}, "", 0);
+
+	//auto cvar = cvarManager->registerCvar("template_cvar", "hello-cvar", "just a example of a cvar");
+	//auto cvar2 = cvarManager->registerCvar("template_cvar2", "0", "just a example of a cvar with more settings", true, true, -10, true, 10 );
+
+	//cvar.addOnValueChanged([this](std::string cvarName, CVarWrapper newCvar) {
+	//	LOG("the cvar with name: {} changed", cvarName);
+	//	LOG("the new value is: {}", newCvar.getStringValue());
+	//});
+
+	//cvar2.addOnValueChanged(std::bind(&CarTracker::YourPluginMethod, this, _1, _2));
+
+	// enabled decleared in the header
+	//enabled = std::make_shared<bool>(false);
+	//cvarManager->registerCvar("TEMPLATE_Enabled", "0", "Enable the TEMPLATE plugin", true, true, 0, true, 1).bindTo(enabled);
+
+	//cvarManager->registerNotifier("NOTIFIER", [this](std::vector<std::string> params){FUNCTION();}, "DESCRIPTION", PERMISSION_ALL);
+	//cvarManager->registerCvar("CVAR", "DEFAULTVALUE", "DESCRIPTION", true, true, MINVAL, true, MAXVAL);//.bindTo(CVARVARIABLE);
+	//gameWrapper->HookEvent("FUNCTIONNAME", std::bind(&TEMPLATE::FUNCTION, this));
+	//gameWrapper->HookEventWithCallerPost<ActorWrapper>("FUNCTIONNAME", std::bind(&CarTracker::FUNCTION, this, _1, _2, _3));
+	//gameWrapper->RegisterDrawable(bind(&TEMPLATE::Render, this, std::placeholders::_1));
+
+
+	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", [this](std::string eventName) {
+	//	LOG("Your hook got called and the ball went POOF");
+	//});
+	// You could also use std::bind here
+	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", std::bind(&CarTracker::YourPluginMethod, this);
+	cvarManager->registerNotifier("CoolerBallOnTop", [this](std::vector<std::string> args) {
+		ballOnTop();
+	}, "", PERMISSION_ALL);
+	cvarManager->registerCvar("cool_enabled", "0", "Enable Cool", true, true, 0, true, 1);
+	cvarManager->registerCvar("cool_distance", "200.0", "Distance to place the ball above");
+	cvarManager->registerCvar("cool_enabled", "0", "Enable Cool", true, true, 0, true, 1)
+		.addOnValueChanged([this](std::string oldValue, CVarWrapper cvar) {
+		coolEnabled = cvar.getBoolValue();
+			});
+}
+
+
+void CarTracker::ballOnTop() {
+	if (!gameWrapper->IsInFreeplay()) { return; }
+	ServerWrapper server = gameWrapper->GetCurrentGameState();
+	if (!server) { return; }
+
+	BallWrapper ball = server.GetBall();
+	if (!ball) { return; }
+	CarWrapper car = gameWrapper->GetLocalCar();
+	if (!car) { return; }
+
+	Vector carVelocity = car.GetVelocity();
+	ball.SetVelocity(carVelocity);
+
+	Vector carLocation = car.GetLocation();
+	float ballRadius = ball.GetRadius();
+
+	CVarWrapper distanceCVar = cvarManager->getCvar("cool_distance");
+	if (!distanceCVar) { return; }
+	float distance = distanceCVar.getFloatValue();
+
+	ball.SetLocation(carLocation + Vector{ 0, 0, distance });
+}
